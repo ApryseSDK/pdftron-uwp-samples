85e49b53b478a1ce8bda8d36b710d4c4aa822f76
Robson Pontin <66638512+pontin-pdftron@users.noreply.github.com>
2021-02-17 22:30:28 -0800

    [PDFViewerControl] added new sample which implements ViewerControl
    [PDFViewerUWPDemo] updated latest PDFTron's SDK
                       removed AppBar's compact height value override

---
+++ PDFViewerControl/PDFViewerControl/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using pdftron.PDF.Tools.Controls.Viewer;
+using PDFViewerUWP_PDFTron.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace PDFViewerControl.ViewModel
+{
+    class MainPageViewModel : BaseViewModel
+    {
+        public MainPageViewModel()
+        {
+            _ = Init(); // Init viewer control with sample PDF file
+        }
+
+        private async Task Init()
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Resources/GettingStarted.pdf"));
+            if (file != null)
+                await ViewerControl.ActivateWithFileAsync(file);
+        }
+
+        ViewerControl _viewerControl = new ViewerControl();
+        public ViewerControl ViewerControl 
+        {
+            get { return _viewerControl; }
+            set { _viewerControl = value; NotifyPropertyChanged(); }
+        }
+    }
+}
